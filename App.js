65422977c0a8b5cb9ec3dfb0382cebf628a52da4
victorbruce <victorbruce82@gmail.com>
2019-12-07 08:34:17 +0000

    Add profile screen

--- App.js
+++ App.js
@@ -1,50 +1,75 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Icon from 'react-native-vector-icons/Ionicons'
 
+// Screens
 import Explore from './screens/Explore';
 import Saved from './screens/Saved';
 import Trips from './screens/Trips';
 import Inbox from './screens/Inbox';
+import Profile from "./screens/Profile";
 
-const App = (props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
+// Bottom Tab Navigator
 const TabNavigator = createBottomTabNavigator({
   Explore: {
     screen: Explore,
     navigationOptions: {
-      tabBarLabel: 'Explore',
+      tabBarLabel: 'EXPLORE',
       tabBarIcon: ({tintColor}) => (
-        <Icon name="ios-search-outline" color={tintColor} size={24} />
+        <Icon name="ios-search" color={tintColor} size={24} />
       )
     }
   },
   Saved: {
-    screen: Saved
+    screen: Saved,
+    navigationOptions: {
+      tabBarLabel: 'SAVED',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-heart-empty" color={tintColor} size={24} />
+      )
+    }
   },
   Trips: {
-    screen: Trips
+    screen: Trips,
+    navigationOptions: {
+      tabBarLabel: 'TRIPS',
+      tabBarIcon: ({tintColor}) => (
+        <Image source={require('./assets/airbnb.png')} style={{height: 24, width: 24, tintColor: tintColor}} />
+      )
+    }
   },
   Inbox: {
-    screen: Inbox
+    screen: Inbox,
+    navigationOptions: {
+      tabBarLabel: 'INBOX',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-chatboxes" color={tintColor} size={24} />
+      )
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: 'PROFILE',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-person" color={tintColor} size={24} />
+      )
+    }
+  }
+},
+// Styling the tab
+{
+  tabBarOptions: {
+    activeTintColor: 'red',
+    inactiveTintColor: 'grey',
+    style: {
+      backgroundColor: 'white',
+      borderTopWidth: 0,
+      elevation: 5
+    }
   }
 })
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default createAppContainer(TabNavigator);
