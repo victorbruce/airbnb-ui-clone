732d93609d176b0f8e2479be1fcd3aa82daae507
victorbruce <victorbruce82@gmail.com>
2019-12-07 09:51:25 +0000

    Add drop shadow style for ios devices

--- App.js
+++ App.js
@@ -67,6 +67,9 @@ const TabNavigator = createBottomTabNavigator({
     style: {
       backgroundColor: 'white',
       borderTopWidth: 0,
+      shadowOffset:{width: 5, height: 3},
+      shadowColor: 'black',
+      shadowOpacity: 0.5,
       elevation: 5
     }
   }
