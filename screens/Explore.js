fd365dcb5c1d0f92016c400fd95b267cd01d7b28
victorbruce <victorbruce82@gmail.com>
2019-12-07 09:50:49 +0000

    Add header to explore screen

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, SafeAreaView, TextInput, Platform} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import SafeViewAndroid from '../components/SafeViewAndroid';
 
 const Explore = () => {
   return (
-    <View style={styles.container}>
-      <Text>Explore</Text>
-    </View>
+    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+      <View style={{ flex: 1 }}>
+        <View style={{ height: 80, backgroundColor: 'white', borderBottomWidth: 1, borderBottomColor: '#ddd'}}>
+          <View style={{
+              flexDirection: 'row', padding: 10, backgroundColor: 'white', marginHorizontal: 20, shadowOffset:{width: 0, height: 0}, shadowColor: 'black', shadowOpacity: 0.2, elevation: 1, marginTop: Platform.Os === 'android' ? 30 : null
+          }}>
+            <Icon name="ios-search" size={20} style={{marginRight: 20}}/>
+            <TextInput 
+              underlineColorAndroid='transparent'
+              placeholder="Try Accra" 
+              placeholderTextColor="grey"
+              style={{ flex: 1, fontWeight: '700', backgroundColor: 'white' }}
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default Explore;
